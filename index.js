10bf3385186b73d35bbf65290eb3f3da51835015
Tony Lam <tony.lam@iki.fi>
2024-09-26 23:13:51 +0300

    Update: working api with express

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+const express = require("express");
 const { chromium } = require("playwright");
 const fs = require("fs");
 
-(async () => {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
+const app = express();
+const port = 3000;
 
-    await page.goto("https://www.alko.fi/tuotteet/000706/Jaloviina-/");
+app.get("/", async (req, res) => {
+    try {
+        const browser = await chromium.launch();
+        const page = await browser.newPage();
 
-    const content = await page.content();
-    const spanElement = await page.$(
-        'span.js-price-container.price-wrapper.price.module-price[itemprop="price"]'
-    );
+        await page.goto("https://www.alko.fi/tuotteet/000706/");
 
-    console.log(spanElement);
+        const content = await page.content();
+        const spanElement = await page.$(
+            'span.js-price-container.price-wrapper.price.module-price[itemprop="price"]'
+        );
+        const spanContent = await spanElement.getAttribute("content");
 
-    fs.writeFileSync("alkopage.txt", content);
+        fs.writeFileSync("alkopage.txt", content);
 
-    await browser.close();
-})();
+        await browser.close();
+
+        res.json({ price: spanContent });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
